3d9308b271123536e97205ec6d8c2dd047c3a5b8
evoexo <dragon520111back@gmail.com>
2025-04-03 09:21:24 -0400

    Added / removed comments

--- Homework/Homework 1/hw1.s
+++ Homework/Homework 1/hw1.s
@@ -1,6 +1,6 @@
 _start:
     # Test code
-    lui s2, 0  # Load upper 20 bits of 0xFFFFFF into s2, resulting in 0xFFFFF000
+    lui s2, 0
     lui s3, 0         
     jal ra, nor
 
@@ -13,10 +13,10 @@ _start:
 nor:
     or s1, s2, s3               # Perform bitwise OR on registers s2 and s3, result in s1
     xori s1, s1, 0xFFFFFFFF     # XOR the value in s1 with 0xFFFFFFFF to flip all bits
-    ret                         # Return from the function
+    ret                         # Return from the function to ra
 
 # Part b.
 nand:
     and s1, s2, s3              # Perform bitwise AND on registers s2 and s3, result in s1
     xori s1, s1, 0xFFFFFFFF     # XOR the value in s1 with 0xFFFFFFFF to flip all bits
-    ret                         # Return from the function to (ra)
+    ret                         # Return from the function to ra
