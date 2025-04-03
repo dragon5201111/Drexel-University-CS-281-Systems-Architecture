c2be2a42225f72115a432b006085e82e1885ade0
evoexo <dragon520111back@gmail.com>
2025-04-03 09:44:08 -0400

    Refactoring

--- Homework/Homework 1/hw1.s
+++ Homework/Homework 1/hw1.s
@@ -1,13 +1,15 @@
+.globl _start
 _start:
     # Test code
     lui s2, 0
-    lui s3, 0         
+    lui s3, 0
+
     jal ra, nor
+    jal ra, nand
 
     li a0, 0            # Set exit code to 0
     li a7, 93           # Set system call number for exit
     ecall               # make system call
-
 
 # Part a.
 nor:
